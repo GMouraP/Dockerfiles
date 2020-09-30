1ce768137a0b222a795754213198fcfdf6530392
Gustavo Moura <gusdev77@gmail.com>
2020-09-30 14:27:55 -0300

    improve start-react-app.sh to make container ready for any UID

--- create-react-app/start-react-app.sh
+++ create-react-app/start-react-app.sh
@@ -2,22 +2,35 @@
 
 export CHOKIDAR_USEPOLLING=1
 
-if [ ! "$(cat /etc/passwd | grep -i ${HUNAME})" ]; then
-    adduser -D -u ${HUID} -s /bin/sh -g "" -h /home/${HUNAME} ${HUNAME}
-    chown -R ${HUID}:${HGID} /usr/local/lib/node_modules
-    chown -R ${HUID}:${HGID} /react-app
-    chown -R ${HUID}:${HGID} /tmp/react-app
+function getUser () {
+    getent passwd "$1" | cut -d: -f1 ;
+}
+
+if [ $(id -u node) != ${HUID} ]; then
+    adduser -D -u ${HUID} -s /bin/sh -g "" -h /home/developer developer
+fi
+
+# If the "/react-app" folder does not exist in the host computer before running the container,
+# Docker will create one owned by root, so we change it to ${HUID} if that happens!
+if [ $(stat -c "%u" /react-app) == 0 ]; then
+    chown ${HUID}:${HGID} /react-app
 fi
 
 if [ -z "$(ls .)" ]; then
-    rsync --chown=${HUID}:${HGID} -a -v --ignore-existing /tmp/react-app/* . 
+    rsync --chown=${HUID}:${HGID} -a -v --ignore-existing /tmp/react-app/* .
 fi
 
 if [ ! -d "./node_modules" ]; then
-    su -c 'npm install' ${HUNAME}
-    su -c 'npm cache clean --force' ${HUNAME}
+    su -c 'npm install' "$(getUser ${HUID})"
+    su -c 'npm cache clean --force' "$(getUser ${HUID})"
 fi
 
-su -c 'npm start' ${HUNAME}
+echo -n "Fixing broken ownerships... "
+find /usr/local/lib/node_modules \! -user ${HUID} -exec chown ${HUID}:${HGID} {} \;
+find /react-app \! -user ${HUID} -exec chown ${HUID}:${HGID} {} \;
+find /tmp/react-app \! -user ${HUID} -exec chown ${HUID}:${HGID} {} \;
+echo -e "Done."
+
+su -c 'npm start' "$(getUser ${HUID})"
 
 exit 0
